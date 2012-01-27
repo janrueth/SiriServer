36e85b4f267df7538f994f281753a2fdf848788a
Eichhoernchen <EHoernchen@gmail.com>
2012-01-27 18:42:35 +0100

    Fixed calendar and reminders

--- siriObjects/reminderObjects.py
+++ siriObjects/reminderObjects.py
@@ -6,11 +6,14 @@ class ReminderSnippet(AceObject):
         self.reminders = reminders if reminders != None else []
         self.temporary = temporary
         self.dialogPhase = dialogPhase
+    
     def to_plist(self):
         self.add_property('reminders')
         self.add_property('temporary')
         self.add_property('dialogPhase')
         return super(ReminderSnippet, self).to_plist()
+
+
 class ReminderObject(AceObject):
     def __init__(self, dueDateTimeZoneId="America/Chicago", dueDate=None, completed=False, lists=None, trigger=None, subject="", important=False, identifier=""):
         super(ReminderObject, self).__init__("Object", "com.apple.ace.reminder")
@@ -22,6 +25,7 @@ class ReminderObject(AceObject):
         self.subject = subject
         self.important = important
         self.identifier = identifier
+    
     def to_plist(self):
         self.add_property('dueDateTimeZoneId')
         self.add_property('dueDate')
@@ -32,17 +36,24 @@ class ReminderObject(AceObject):
         self.add_property('important')
         self.add_property('identifier')
         return super(ReminderObject, self).to_plist()
+
+
 class ListObject(AceObject):
     def __init__(self, name = "Tasks"):
         super(ListObject, self).__init__("ListObject", "com.apple.ace.reminder")
         self.name = name
+    
     def to_plist(self):
         self.add_property('name')
         return super(ListObject, self).to_plist()
+
+
 class DateTimeTrigger(AceObject):
     def __init__(self, date=None):
         super(DateTimeTrigger, self).__init__("DateTimeTrigger", "com.apple.ace.reminder")
         self.date = date
+    
     def to_plist(self):
         self.add_property('date')
         return super(DateTimeTrigger, self).to_plist()
+
