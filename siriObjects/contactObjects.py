84b22119fbbf464e3544363c81290e6a7dcec63e
Eichhoernchen <EHoernchen@gmail.com>
2012-01-27 18:58:28 +0100

    Fixed contacts

--- siriObjects/contactObjects.py
+++ siriObjects/contactObjects.py
@@ -1,13 +1,15 @@
 from siriObjects.baseObjects import ClientBoundCommand, AceObject
 
 class PersonSearch(ClientBoundCommand):
-    def __init__(self, name="", scope="Local"):
-        super(PersonSearch, self).__init__("PersonSearch", "com.apple.ace.contact")
+    def __init__(self, refId, name="", scope="Local"):
+        super(PersonSearch, self).__init__("PersonSearch", "com.apple.ace.contact", None, refId)
         self.name = name
         self.scope = scope
+    
     def to_plist(self):
         self.add_property('name')
         self.add_property('scope')
         return super(PersonSearch, self).to_plist()
+
     
 # TODO: Missing objects for use in creating or updating a contact.
