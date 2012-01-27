72fa27b1adc8553a76f7d22ff934ec4233aec168
Cale Cunningham <fspilotcale@gmail.com>
2012-01-27 10:20:33 -0600

    Oops. Editor didn't save when it said it did.

--- siriObjects/systemObjects.py
+++ siriObjects/systemObjects.py
@@ -30,53 +30,19 @@ class DomainObjectCommit(ClientBoundCOmmand):
     def to_plist(self):
         self.add_property('identifier')
         return super(DomainObjectCommit, self).to_plist()
-    
-# Calendar Event-related objects
-class EventSearch(ClientBoundCommand):
-    def __init__(self, timeZoneId="America/Chicago", startDate=None, endDate=None, limit=10):
-        super(EventSearch, self).__init__("EventSearch", "com.apple.ace.calendar")
-        self.timeZoneId = timeZoneId
-        self.startDate = startDate
-        self.endDate = endDate
-        self.limit = limit
+class SendCommands(AceObject):
+    def __init__(self, commands=None):
+        super(SendCommands, self).__init__("SendCommands", "com.apple.ace.system")
+        self.commands = commands if commands != None else []
     def to_plist(self):
-        self.add_property('timeZoneId')
-        self.add_property('startDate')
-        self.add_property('endDate')
-        self.add_property('limit')
-        return super(EventSearch, self).to_plist()
-class EventObject(AceObject):
-    def __init__(self, timeZoneId="America/Chicago", title="", startDate=None, endDate=None):
-        super(EventObject, self).__init__("Event", "com.apple.ace.calendar")
-        self.timeZoneId = timeZoneId
-        self.title = title
-        self.startDate = startDate
-        self.endDate = endDate
+        self.add_property('commands')
+        return super(SendCommands, self).to_plist()
+class StartRequest(AceObject):
+    def __init__(self, handsFree=False, utterance=""):
+        super(StartRequest, self).__init__("StartRequest", "com.apple.ace.system")
+        self.handsFree = handsFree
+        self.utterance = utterance
     def to_plist(self):
-        self.add_property('timeZoneId')
-        self.add_property('title')
-        self.add_property('startDate')
-        self.add_property('endDate')
-        return super(EventObject, self).to_plist()
-    
-# Contacts-related objects
-class PersonSearch(ClientBoundCommand):
-    def __init__(self, name="", scope="Local"):
-        super(PersonSearch, self).__init__("PersonSearch", "com.apple.ace.contact")
-        self.name = name
-        self.scope = scope
-    def to_plist(self):
-        self.add_property('name')
-        self.add_property('scope')
-        return super(PersonSearch, self).to_plist()
-    
-# Alarm-related objects
-class AlarmSearch(ClientBoundCommand):
-    def __init__(self, minute=0, hour=0):
-        super(AlarmSearch, self).__init__("Search", "com.apple.ace.alarm")
-        self.minute = minute
-        self.hour = hour
-    def to_plist(self):
-        self.add_property('minute')
-        self.add_property('hour')
-        return super(AlarmSearch, self).to_plist()
+        self.add_property('handsFree')
+        self.add_property('utterance')
+        return super(StartRequest, self).to_plist()
