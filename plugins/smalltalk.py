3f49494eac9f7a384153c1b90ecbb689fcbb4a22
Joh Gerna <johge@gmx.net>
2012-02-03 12:21:41 +0100

    A little bit small talk for Siri, to be not so boring. Sorry about the english part.

---
+++ plugins/smalltalk.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from plugin import *
+
+class smalltalk(Plugin):
+    
+    @register("de-DE", "(.*Hallo.*)|(.*Hi.*Siri.*)")
+    @register("en-US", "(.*Hello.*)|(.*Hi.*Siri.*)")
+    def st_hello(self, speech, language):
+        if language == 'de-DE':
+         self.say("Hallo.")
+        else:
+            self.say("Hello")
+        self.complete_request()
+
+    @register("de-DE", ".*Dein Name.*")
+    @register("en-US", ".*your name.*")
+    def st_name(self, speech, language):
+        if language == 'de-DE':
+         self.say("Siri.")
+        else:
+            self.say("Siri.")
+        self.complete_request()
+
+    @register("de-DE", "Wie geht es dir?")
+    @register("en-US", "How are you?")
+    def st_howareyou(self, speech, language):
+        if language == 'de-DE':
+         self.say("Gut danke der Nachfrage.")
+        else:
+            self.say("Fine, thanks for asking!")
+        self.complete_request()
+        
+    @register("de-DE", ".*Danke.*")
+    @register("en-US", ".*Thank.*you.*")
+    def st_thank_you(self, speech, language):
+        if language == 'de-DE':
+            self.say("Bitte.")
+            self.say("Kein Ding.")
+        else:
+            self.say("You are welcome.")
+            self.say("This is my job.")
+        self.complete_request()     
+    
+    @register("de-DE", "(.*möchtest.*heiraten.*)|(.*willst.*heiraten.*)")
+    @register("en-US", ".*Whant.*marry*")
+    def st_marry_me(self, speech, language):
+        if language == 'de-DE':
+            self.say("Nein Danke, ich stehe auf das schwarze iPhone von Deinem Kollegen.")            
+        else:
+            self.say("No thank you, I'm in love with the black iPhone from you friend.")
+        self.complete_request()
+
+    @register("de-DE", ".*erzähl.*Witz.*")
+    @register("en-US", ".*tell.*joke*")
+    def st_tell_joke(self, speech, language):
+        if language == 'de-DE':
+            self.say("Zwei iPhones stehen an der Bar ... den Rest habe ich vergessen.")            
+        else:
+            self.say("Two iPhones walk into a bar ... I forget the rest.")
+        self.complete_request()
+
+    @register("de-DE", ".*erzähl.*Geschichte.*")
+    @register("en-US", ".*tell.*story*")
+    def st_tell_story(self, speech, language):
+        if language == 'de-DE':
+            self.say("Es war einmal ... nein, es ist zu albern")            
+        else:
+            self.say("Far far away, there was ... no, too stupid")
+        self.complete_request()
+
+    @register("de-DE", "(.*Was trägst Du?.*)|(.*Was.*hast.*an.*)")
+    @register("en-US", ".*what.*wearing*")
+    def st_tell_clothes(self, speech, language):
+        if language == 'de-DE':
+            self.say("Das kleine schwarze oder war es das weiße?")
+            self.say("Bin morgends immer so neben der Spur.")  
+        else:
+            self.say("I guess the small black one, or was it white?")
+        self.complete_request()
+
+    @register("de-DE", ".*Bin ich dick.*")
+    @register("en-US", ".*Am I fat*")
+    def st_fat(self, speech, language):
+        if language == 'de-DE':
+            self.say("Dazu möchte ich nichts sagen.")            
+        else:
+            self.say("I would prefer not to say.")
+        self.complete_request()
