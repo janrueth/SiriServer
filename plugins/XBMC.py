e20252816989066af123123bc0961c57e2317874
gugahoi <guga@maurorisch.com.br>
2012-02-04 11:17:11 +1100

    fixed indentation

--- plugins/XBMC.py
+++ plugins/XBMC.py
@@ -53,24 +53,23 @@ class XBMC(Plugin):
                 self.say("XBMC URL: %s" %(xbmc.get_url()))
             elif command == 'shut down' or command == 'shutdown' or command == 'turn off':
                 self.say("XBMC going down")
-        json.System.Shutdown()
-        elif command == 'boot' or command == 'start' or command == 'boot up':
-            addr_byte = xbmc.mac_address.split(':')
-            hw_addr = struct.pack('BBBBBB',
-            int(addr_byte[0], 16),
-            int(addr_byte[1], 16),
-            int(addr_byte[2], 16),
-            int(addr_byte[3], 16),
-            int(addr_byte[4], 16),
-            int(addr_byte[5], 16))
-            msg = '\xff' * 6 + hw_addr * 16
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            s.sendto(msg, ("255.255.255.255", 9))
-        else:
+                json.System.Shutdown()
+            elif command == 'boot' or command == 'start' or command == 'boot up':
+                addr_byte = xbmc.mac_address.split(':')
+                hw_addr = struct.pack('BBBBBB',
+                int(addr_byte[0], 16),
+                int(addr_byte[1], 16),
+                int(addr_byte[2], 16),
+                int(addr_byte[3], 16),
+                int(addr_byte[4], 16),
+                int(addr_byte[5], 16))
+                msg = '\xff' * 6 + hw_addr * 16
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                s.sendto(msg, ("255.255.255.255", 9))
+            else:
                 self.say("XBMC command not recognized: %s."%(command))
-
         else:
-            self.say("XBMC currently supports the following commands: play, pause, stop and info.")
+            self.say("XBMC currently supports the following commands: play [movie], pause, stop, shutdown, start and info.")
         
         self.complete_request()
