e093deba951e9d9fdd57f819e4115205c092dc5a
gugahoi <guga@maurorisch.com.br>
2012-02-05 18:48:23 +1100

    Deleted M2Crypto lib

--- lib/M2Crypto/callback.py
+++
@@ -1,9 +0,0 @@
-"""Deprecated, use the util module instead.
-
-Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
-
-import warnings
-
-warnings.warn('Use the util module instead', DeprecationWarning)
-
-from util import genparam_callback, passphrase_callback
