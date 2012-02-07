769917401225a8bbd26a5eca00729f3c1a0a83db
ArtyMig <new.arthur.migdal@gmail.com>
2012-02-07 18:17:33 -0500

    Added german language, and "internet" query.

--- plugins/wwwsearch.py
+++ plugins/wwwsearch.py
@@ -5,17 +5,38 @@ from plugin import *
 from siriObjects.websearchObjects import WebSearch
 
 class wwwSearch(Plugin):
-
-    @register("en-US", "(web search.*)|(web.*)")
+    
+    @register("de-DE", "(websuche.*)|(web suche.*)|(internetsuche.*)|(internet suche.*)|(web.*)|(internet.*)")
+    @register("en-US", "(web search.*)|(web.*)|(internet.*)|(internet search.*)")
     def webSearch(self, speech, language):
-
-        if (speech.find('Web search') == 0):
-            speech = speech.replace('Web search', ' ',1)
-        elif (speech.find('Web') == 0):
-            speech = speech.replace('Web',' ',1)
-        speech = speech.strip()
-        if speech == "":
-            speech = self.ask("What is your query?")
+        if (language == "en-US"):
+            if (speech.find('Web search') == 0):
+                speech = speech.replace('Web search', ' ',1)
+            elif (speech.find('Web') == 0):
+                speech = speech.replace('Web',' ',1)
+            elif (speech.find('Internet search') == 0):
+                speech = speech.replace('Internet search',' ',1)
+            elif (speech.find('Internet') == 0):
+                speech = speech.replace('Internet',' ',1)
+            speech = speech.strip()
+            if speech == "":
+                speech = self.ask("What is your query?")
+        elif (language == "de-DE"):
+            if (speech.find('Websuche') == 0):
+                speech = speech.replace('Websuche',' ',1)
+            elif (speech.find('Web suche') == 0):
+                speech = speech.replace('Web suche',' ',1)
+            elif (speech.find('Internetsuche') == 0):
+                speech = speech.replace('Internetsuche',' ',1)
+            elif (speech.find('Internet suche') == 0):
+                speech = speech.replace('Internet suche',' ',1)
+            elif (speech.find('Web') == 0):
+                speech = speech.replace('Web',' ',1)
+            elif (speech.find('Internet') == 0):
+                speech = speech.replace('Internet',' ',1)
+            speech = speech.strip()
+            if speech == "":
+                speech = self.ask("Was ist Ihre Frage?")
 
         search = WebSearch(refId="", aceId="", query=speech)
         self.send_object(search)
