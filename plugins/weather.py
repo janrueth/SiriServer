0462cb0de940be22e4c475b0ff88169cedeb0d9f
Eichhoernchen <EHoernchen@gmail.com>
2012-02-07 12:51:00 +0100

    seabass86 merge

--- plugins/weather.py
+++ plugins/weather.py
@@ -74,6 +74,7 @@ class weatherPlugin(Plugin):
             self.say("Please obtain an API key from http://api.wunderground.com/weather/api/ and enter it in line 17!")
             self.complete_request()
             return False
+	speech = speech.replace(u".","")
         viewType ="DAILY"
         if (speech.count("today") > 0 or speech.count("current") > 0 or speech.count(" for today") > 0) and language=="en-US":
             viewType = "HOURLY"
