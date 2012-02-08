046f9c8d464c6186f1ef7baf97fbc5c33f18db65
onetootreefor <onetootreefor@ks370279.kimsufi.com>
2012-02-09 00:42:41 +0100

    updated wwwsearch.py

---
+++ plugins/wwwsearch.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python                                                                                                                                                                   
+# -*- coding: utf-8 -*-                                                                                                                                                             
+
+from plugin import *
+from siriObjects.websearchObjects import WebSearch
+
+class wwwSearch(Plugin):
+
+    @register("en-US", "(search web.*)|(web.*)")
+    def webSearch(self, speech, language):
+
+        if (speech.find('search web') == 0):
+            speech = speech.replace('Web search', '',1)
+        elif (speech.find('Web') == 0):
+            speech = speech.replace('Web','',1)
+        speech = speech.strip()
+        if speech == "":
+            speech = self.ask("What is your query?")
+
+        search = WebSearch(refId="", aceId="", query=speech)
+        self.send_object(search)
+        self.complete_request()
+
