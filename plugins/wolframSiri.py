359c1ca50d447fd46e0dd82c3233aa82495410d3
Juraj K <admin@game-host.eu>
2012-02-09 17:39:48 +0100

    Added Wolram Alpha compatible with new API key storage. STABLE

--- plugins/wolframSiri.py
+++ plugins/wolframSiri.py
@@ -17,41 +17,13 @@ from plugin import *
 
 from siriObjects.baseObjects import AceObject, ClientBoundCommand
 from siriObjects.uiObjects import AddViews, AssistantUtteranceView
+from siriObjects.answerObjects import AnswerSnippet, AnswerObject, AnswerObjectLine
 
-APPID = "" #Add your APPID between the ""
 
-class SiriAnswerSnippet(AceObject):
-    def __init__(self, answers=None):
-        super(SiriAnswerSnippet, self).__init__("Snippet", "com.apple.ace.answer")
-        self.answers = answers if answers != None else []
-        self.confirmationOptions = None
-    
-    def to_plist(self):
-        self.add_property('answers')
-        self.add_property('confirmationOptions')
-        return super(SiriAnswerSnippet, self).to_plist()
+APPID = APIKeyForAPI("wolframalpha")
 
-class SiriAnswer(AceObject):
-    def __init__(self, title=None, lines=None):
-        super(SiriAnswer, self).__init__("Object", "com.apple.ace.answer")
-        self.title = title
-        self.lines = lines if lines != None else []
-    
-    def to_plist(self):
-        self.add_property('title')
-        self.add_property('lines')
-        return super(SiriAnswer, self).to_plist()
 
-class SiriAnswerLine(AceObject):
-    def __init__(self, text="", image=""):
-        super(SiriAnswerLine, self).__init__("ObjectLine", "com.apple.ace.answer")
-        self.text = text
-        self.image = image
-    
-    def to_plist(self):
-        self.add_property('text')
-        self.add_property('image')
-        return super(SiriAnswerLine, self).to_plist()
+
 
 class wolfram(Plugin):
     
@@ -174,44 +146,44 @@ class wolfram(Plugin):
         view = AddViews(self.refId, dialogPhase="Completion")
         if wolfram_pod0 != 12:
             if wolfram0_img == 1:
-                wolframAnswer = SiriAnswer(title=wolfram_pod0,lines=[SiriAnswerLine(image=wolfram0)])
+                wolframAnswer = AnswerObject(title=wolfram_pod0,lines=[AnswerObjectLine(image=wolfram0)])
             else:
-                wolframAnswer = SiriAnswer(title=wolfram_pod0,lines=[SiriAnswerLine(text=wolfram0)])
+                wolframAnswer = AnswerObject(title=wolfram_pod0,lines=[AnswerObjectLine(text=wolfram0)])
         else:
             print wolfram_pod0
         if wolfram_pod1 != 12:
             if wolfram1_img == 1:
-                wolframAnswer1 = SiriAnswer(title=wolfram_pod1,lines=[SiriAnswerLine(image=wolfram1)])
+                wolframAnswer1 = AnswerObject(title=wolfram_pod1,lines=[AnswerObjectLine(image=wolfram1)])
             else:
-                wolframAnswer1 = SiriAnswer(title=wolfram_pod1,lines=[SiriAnswerLine(text=wolfram1)])
+                wolframAnswer1 = AnswerObject(title=wolfram_pod1,lines=[AnswerObjectLine(text=wolfram1)])
         else:
             print wolfram_pod1
         if wolfram_pod2 != 12:
             if wolfram2_img == 1:
-                wolframAnswer2 = SiriAnswer(title=wolfram_pod2,lines=[SiriAnswerLine(image=wolfram2)])
+                wolframAnswer2 = AnswerObject(title=wolfram_pod2,lines=[AnswerObjectLine(image=wolfram2)])
             else:
-                wolframAnswer2 = SiriAnswer(title=wolfram_pod2,lines=[SiriAnswerLine(text=wolfram2)])
+                wolframAnswer2 = AnswerObject(title=wolfram_pod2,lines=[AnswerObjectLine(text=wolfram2)])
         else:
             print wolfram_pod2
         if wolfram_pod3 != 12:
             if wolfram3_img == 1:
-                wolframAnswer3 = SiriAnswer(title=wolfram_pod3,lines=[SiriAnswerLine(image=wolfram3)])
+                wolframAnswer3 = AnswerObject(title=wolfram_pod3,lines=[AnswerObjectLine(image=wolfram3)])
             else:
-                wolframAnswer3 = SiriAnswer(title=wolfram_pod3,lines=[SiriAnswerLine(text=wolfram3)])
+                wolframAnswer3 = AnswerObject(title=wolfram_pod3,lines=[AnswerObjectLine(text=wolfram3)])
         else:
             print wolfram_pod3
         if wolfram_pod4 != 12:
             if wolfram4_img == 1:
-                wolframAnswer4 = SiriAnswer(title=wolfram_pod4,lines=[SiriAnswerLine(image=wolfram4)])
+                wolframAnswer4 = AnswerObject(title=wolfram_pod4,lines=[AnswerObjectLine(image=wolfram4)])
             else:
-                wolframAnswer4 = SiriAnswer(title=wolfram_pod4,lines=[SiriAnswerLine(text=wolfram4)])
+                wolframAnswer4 = AnswerObject(title=wolfram_pod4,lines=[AnswerObjectLine(text=wolfram4)])
         else:
             print wolfram_pod4
         if wolfram_pod8 != 12:
             if wolfram8_img == 1:
-                wolframAnswer8 = SiriAnswer(title=wolfram_pod8,lines=[SiriAnswerLine(image=wolfram8)])
+                wolframAnswer8 = AnswerObject(title=wolfram_pod8,lines=[AnswerObjectLine(image=wolfram8)])
             else:
-                wolframAnswer8 = SiriAnswer(title=wolfram_pod8,lines=[SiriAnswerLine(text=wolfram8)])
+                wolframAnswer8 = AnswerObject(title=wolfram_pod8,lines=[AnswerObjectLine(text=wolfram8)])
         if wolfram_pod0 == 12:
             if APPID == "":
                 self.say("Sorry I can't process your request. Your APPID is not set! Please register free dev account at http://wolframalpha.com and edit line 21 with you APPID.")
@@ -223,17 +195,17 @@ class wolfram(Plugin):
             self.complete_request()
             view1 = 0
         elif wolfram_pod1 == 12:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer])
+            view1 = AnswerSnippet(answers=[wolframAnswer])
         elif wolfram_pod2 == 12:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer, wolframAnswer1])
+            view1 = AnswerSnippet(answers=[wolframAnswer, wolframAnswer1])
         elif wolfram_pod3 == 12:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2])
+            view1 = AnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2])
         elif wolfram_pod4 == 12:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3])
+            view1 = AnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3])
         elif wolfram_pod8 == 12:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3, wolframAnswer4])
+            view1 = AnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3, wolframAnswer4])
         else:
-            view1 = SiriAnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3, wolframAnswer4, wolframAnswer8])
+            view1 = AnswerSnippet(answers=[wolframAnswer, wolframAnswer1, wolframAnswer2, wolframAnswer3, wolframAnswer4, wolframAnswer8])
         view.views = [view1]
         self.sendRequestWithoutAnswer(view)
         self.complete_request()
