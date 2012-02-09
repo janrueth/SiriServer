052a22d50d772a13a665af18a6a67ba98d010dcf
Cedric Boverie <ced@boverie.eu>
2012-02-09 21:01:58 +0100

    Plugin VDM. Sans cle API, il est capricieux...

---
+++ plugins/viedemerde.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import xml.etree.ElementTree as ET
+import urllib2, urllib
+from plugin import *
+
+
+class vieDeMerde(Plugin):
+    
+    @register("fr-FR", "Vie.*(merde|merd)")
+    def fuckMyLife(self, speech, language):
+        vdm = None
+        try:
+            response = urllib2.urlopen("http://api.viedemerde.fr/1.0/view/random", timeout=3).read()
+            vdm = ET.fromstring(response).find("vdms/vdm/texte")
+        except:
+            pass
+
+        if vdm != None:
+            self.say(vdm.text)
+        else:
+            self.say(u"Désolé, l'API Vie De Merde autorise très peu de requête.")
+        self.complete_request()
