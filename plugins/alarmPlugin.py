2cd1dcda6cb80c78f16b6b28e8e2f6908449e0a6
Cedric Boverie <ced@boverie.eu>
2012-02-10 17:27:20 +0100

    Restore en-US compat

--- plugins/alarmPlugin.py
+++ plugins/alarmPlugin.py
@@ -9,20 +9,15 @@
 #          set an alarm for HH AM/PM <called/labeled/named> <[word 1] [word 2] [word 3]>
 #comments: feel free to email any comments/bug/updates
 
-
 import re
-
 from fractions import Fraction
-
 from plugin import *
-
 from siriObjects.baseObjects import AceObject, ClientBoundCommand
 from siriObjects.uiObjects import AddViews, AssistantUtteranceView
 from siriObjects.systemObjects import DomainObject
 from siriObjects.alarmObjects import *
 
 class alarmPlugin(Plugin):
-
     localizations = {
         'Alarm': {
             "settingAlarm": {
@@ -49,30 +44,35 @@ class alarmPlugin(Plugin):
     @register("fr-FR", res['setAlarm']['fr-FR'])
     def setAlarm(self, speech, language):
         alarmString = re.match(alarmPlugin.res['setAlarm'][language], speech, re.IGNORECASE)
-		
-		
-		#need to add back english compatibility
-        alarmHour = int(alarmString.group(3))
-        alarm24Hour = alarmHour
-        alarmMinutes = alarmString.group(4)
-        alarmAMPM = ""
-        alarmLabelExists = alarmString.group(6)
-        
+				
+        if language == 'fr-FR':
+            labelGroupId = 6
+            alarmHour = int(alarmString.group(3))
+            alarm24Hour = alarmHour
+            alarmMinutes = alarmString.group(4)
+            alarmAMPM = ""
+            alarmLabelExists = alarmString.group(labelGroupId)
+        else:
+            labelGroupId = 4
+            alarmHour = int(alarmString.group(1))
+            alarm24Hour = alarmHour
+            alarmMinutes = alarmString.group(2)
+            alarmAMPM = alarmString.group(3)
+            alarmLabelExists = alarmString.group(labelGroupId)
+
         #check if we are naming the alarm
         if alarmLabelExists == None:
             alarmLabel = None
         else:
-            alarmLabel = alarmString.group(6)
-        
+            alarmLabel = alarmString.group(labelGroupId)
+
         #the siri alarm object requires 24 hour clock
         if (alarmAMPM == "pm" and alarmHour != 12):
             alarm24Hour += 12
-
         if alarmMinutes == None:
             alarmMinutes = "00"
         else:
             alarmMinutes = int(alarmMinutes.strip())
-
         view = AddViews(self.refId, dialogPhase="Reflection")
         view.views = [
             AssistantUtteranceView(
@@ -83,15 +83,12 @@ class alarmPlugin(Plugin):
         #create the alarm
         alarm = AlarmObject(alarmLabel, int(alarmMinutes), alarm24Hour, None, 1)
         response = self.getResponseForRequest(AlarmCreate(self.refId, alarm))
-        
         print(alarmPlugin.localizations['Alarm']['alarmWasSet'][language].format(alarmHour, alarmMinutes, alarmAMPM))
         view = AddViews(self.refId, dialogPhase="Completion")
-        
         if alarmLabel == None:
             view1 = AssistantUtteranceView(speakableText=alarmPlugin.localizations['Alarm']['alarmWasSet'][language].format(alarmHour, alarmMinutes, alarmAMPM), dialogIdentifier="Alarm#alarmWasSet")
         else:
             view1 = AssistantUtteranceView(speakableText=alarmPlugin.localizations['Alarm']['alarmSetWithLabel'][language].format(alarmLabelExists, alarmLabel, alarmHour, alarmMinutes, alarmAMPM), dialogIdentifier="Alarm#alarmSetWithLabel")
-        
         view2 = AlarmSnippet(alarms=[alarm])
         view.views = [view1, view2]
         self.sendRequestWithoutAnswer(view)
