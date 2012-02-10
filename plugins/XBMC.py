33e35fed0a182e3baeab70b9e7a1bb41a7e14b9a
Pieter Janssens <Pieter>
2012-02-10 15:21:25 +0100

    Added movie/tvshow thumbnails view to siri

--- plugins/XBMC.py
+++ plugins/XBMC.py
@@ -10,11 +10,13 @@
 from plugin import *
 import urllib2, urllib, socket, struct, logging, re
 
+from siriObjects.uiObjects import AddViews
+from siriObjects.answerObjects import AnswerSnippet, AnswerObject, AnswerObjectLine
+
 try: 
     import jsonrpclib
 except ImportError: 
     raise NecessaryModuleNotFound('XBMC plugin will not work: JSONRPCLIB not installed. To install, run "easy_install jsonrpclib"')
-    
 
 class XBMC_object():
     def __init__(self, host='appletv.local', port='8080', username=None, password=None, mac_address=None):
@@ -29,11 +31,27 @@ class XBMC_object():
         
     def get_user_pass(self):
         if self.username != None and self.password != None:
-            return '%s:%s@' % (self.username, self.password)
-        
+            return '%s:%s@' % (self.username, self.password) 
         return ''
+        
+    def get_thumburl(self):
+        return 'http://%s:%s/vfs/' % (self.host, self.port)
 
-class XBMC(Plugin):
+    def play(self,json,item):
+        json.Playlist.Clear(playlistid=1)
+        json.Playlist.Add(playlistid=1, item=item)
+        json.Player.Open({ 'playlistid': 1 })
+
+
+class XBMC(Plugin):	    
+
+    def addPictureView(self,title,image_url):
+        view = AddViews(self.refId, dialogPhase="Completion")
+        ImageAnswer = AnswerObject(title=title,lines=[AnswerObjectLine(image=image_url)])
+        view1 = AnswerSnippet(answers=[ImageAnswer])
+        view.views = [view1]
+        self.sendRequestWithoutAnswer(view)
+	
     global xbmc
     xbmc = XBMC_object()
             
@@ -81,10 +99,12 @@ class XBMC(Plugin):
                         self.say(names, None)
                         self.say("To play the first one add 'first occurrence' at the end of your command")
                     else:
-                        json.Playlist.Clear(playlistid=1)
-                        json.Playlist.Add(playlistid=1, item={ 'movie' + 'id': movieid })
-                        json.Player.Open({ 'playlistid': 1 })
                         self.say('%s starting'%(matches[0]))
+                        details = json.VideoLibrary.GetMovieDetails(movieid=movieid, properties= ['thumbnail','year','rating'])['moviedetails']
+                        image_url = "%s%s" % (xbmc.get_thumburl(),details['thumbnail'])
+                        title = "%s (%s) - %s/10" % (details['label'],details['year'],round(details['rating'],1))
+                        self.addPictureView(title,image_url)
+                        xbmc.play(json,{'movieid': movieid})
                 else:
                     result = json.VideoLibrary.GetTVShows()
                     tvmatches = []
@@ -111,32 +131,33 @@ class XBMC(Plugin):
                             names = ''
                             for x in matches:
                                 names = names + x + '\n'
-                            self.say(names, None)
+                            self.say(names,None)
                         else:
                             result = json.VideoLibrary.GetEpisodes(tvshowid=tvshowid,properties=['playcount','showtitle','season','episode'])
-                            print (result['episodes'])
-                            print (len(result['episodes']))
                             if latest_episode == True:
                                 episode = result['episodes'][len(result['episodes'])-1]
+                                episodeid = episode['episodeid']
+                                play = True
                                 if episode['playcount'] > 0:
-                                    self.say("Warning: it seems that you already watched this episode.")
-                                self.say('Playing %s, season %s, episode %s.' %(episode['showtitle'], episode['season'], episode['episode']))
-                                json.Playlist.Clear(playlistid=1)
-                                json.Playlist.Add(playlistid=1, item={ 'episode' + 'id': episode['episodeid'] })
-                                json.Player.Open({ 'playlistid': 1 })
+                                    self.say("Warning: it seems that you already watched this episode.",None)
                             else: 
                                 allwatched = True
                                 for episode in result['episodes']:
                                     if episode['playcount'] == 0:
                                         episodeid=episode['episodeid']
-                                        self.say('Playing %s, season %s, episode %s.' %(episode['showtitle'], episode['season'], episode['episode']))
-                                        json.Playlist.Clear(playlistid=1)
-                                        json.Playlist.Add(playlistid=1, item={ 'episode' + 'id': episodeid })
-                                        json.Player.Open({ 'playlistid': 1 })
                                         allwatched = False
+                                        play = True
                                         break
                                 if allwatched == True:
                                     self.say('There are no unwatched and/or new episodes of %s' %(title))
+                                    play = False
+                            if play == True:
+                                details = json.VideoLibrary.GetTVShowDetails(tvshowid=tvshowid, properties= ['thumbnail','rating'])['tvshowdetails']
+                                image_url = "%s%s" % (xbmc.get_thumburl(),details['thumbnail'])
+                                title = "%s - %s/10" % (details['label'],round(details['rating'],1))
+                                self.say('Playing %s, season %s, episode %s.' %(episode['showtitle'], episode['season'], episode['episode']))
+                                self.addPictureView(title,image_url)
+                                xbmc.play(json,{ 'episodeid': episodeid })
                     else:
                         self.say('No movies or TV shows matching: %s.' % (title))
             elif command == 'info':
