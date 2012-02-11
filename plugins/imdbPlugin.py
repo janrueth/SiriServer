a95105706d1486cbc27595142a2c9417b9033e49
Cedric Boverie <ced@boverie.eu>
2012-02-12 00:54:44 +0100

    More IMDb fonctions (all from siriProxy and more...)

--- plugins/imdbPlugin.py
+++ plugins/imdbPlugin.py
@@ -7,8 +7,12 @@
 import re
 from plugin import *
 
+from siriObjects.uiObjects import AddViews
+from siriObjects.answerObjects import AnswerSnippet, AnswerObject, AnswerObjectLine
+
 try:
    from imdb import IMDb
+   import imdb.helpers
 except ImportError:
    raise NecessaryModuleNotFound("IMDb library not found. Please install IMDbPy library! e.g. sudo easy_install IMDbPy (or sudo apt-get install python-imdbpy)")
 
@@ -17,7 +21,7 @@ class imdbPlugin(Plugin):
     
     res = {
         'who_is': {
-            'fr-FR': u"(Qui joue dans le film|Qui joue dans|.*acteurs? de|.*acteurs? du film|Qui (es|est|étais|etais|était|etait) dans le film) (.*)",
+            'fr-FR': u"(Qui (joue|a joue|a joué) dans le film|Qui joue dans|.*acteurs? de|.*acteurs? du film|Qui (es|est|étais|etais|était|etait) dans le film) (.*)",
         },
         'search_actor': {
             'fr-FR': u"Je recherche les acteurs du film {0}...",
@@ -52,8 +56,32 @@ class imdbPlugin(Plugin):
         'no_ratings': {
             'fr-FR': u"Je ne trouve pas la notation du film {0}.",
         },
+        'director': {
+            'fr-FR': u"(.*(réalisé|realisé|realise|r(é|e)alisai(s|t)).*film) (.*)",
+        },
+        'search_director': {
+            'fr-FR': u"Je recherche le réalisateur du film {0}...",
+        },
+        'here_director': {
+            'fr-FR': u"{0} a été réalisé par {1}.",
+        },
+        'no_director': {
+            'fr-FR': u"Je ne trouve pas le réalisateur du film {0}.",
+        },
+        'cover': {
+            'fr-FR': u"(.*(affiche|la fiche).*film) (.*)",
+        },
+        'search_cover': {
+            'fr-FR': u"Je recherche l'affiche du film {0}...",
+        },
+        'here_cover': {
+            'fr-FR': u"Voici l'affiche du film {0} :",
+        },
+        'no_cover': {
+            'fr-FR': u"Je ne trouve pas l'affiche du film {0}.",
+        },
         'dont_find_movie': {
-            'fr-FR': u"Je ne trouve le film {0}",
+            'fr-FR': u"Je ne trouve pas le film {0}",
         },
     }
 
@@ -125,5 +153,53 @@ class imdbPlugin(Plugin):
             except:
                 noratings = self.res["no_ratings"][language];
                 self.say(noratings.format(infos["title"]))
+
+        self.complete_request()
+
+    @register("fr-FR", res["director"]["fr-FR"])
+    def moviedirector(self, speech, language, regex):
+
+        query = regex.group(regex.lastindex).strip()
+        search_director = self.res["search_director"][language]
+        self.say(search_director.format(query))
+        infos = self.searchMovie(query,language)
+        
+        if infos != None:
+            try:
+                here_director = self.res["here_director"][language];
+                self.say(here_director.format(infos["title"],infos["director"][0]))
+            except:
+                nodirector = self.res["no_director"][language];
+                self.say(nodirector.format(infos["title"]))
+
+        self.complete_request()
+
+    @register("fr-FR", res["cover"]["fr-FR"])
+    def moviecover(self, speech, language, regex):
+
+        query = regex.group(regex.lastindex).strip()
+        search = self.res["search_cover"][language]
+        self.say(search.format(query))
+        infos = self.searchMovie(query,language)
+        
+        ImageURL = None
+        if infos != None:
+            try:
+                here = self.res["here_cover"][language];
+                self.say(here.format(infos["title"]))
+                ImageURL = infos["cover url"]
+            except:
+                no = self.res["no_cover"][language];
+                self.say(no.format(infos["title"]))
+
+        if ImageURL != None:
+            ImageUrlBig = imdb.helpers.fullSizeCoverURL(infos)
+            if ImageUrlBig != None:
+                ImageURL = ImageUrlBig
+            view = AddViews(self.refId, dialogPhase="Completion")
+            ImageAnswer = AnswerObject(title=infos["title"],lines=[AnswerObjectLine(image=ImageURL)])
+            view1 = AnswerSnippet(answers=[ImageAnswer])
+            view.views = [view1]
+            self.sendRequestWithoutAnswer(view)
 
         self.complete_request()
