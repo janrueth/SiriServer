1a84047672afc53f13392155bffaaf8b5f384629
Cedric Boverie <ced@boverie.eu>
2012-02-12 16:21:04 +0100

    Random answer in examplePlugin

--- plugins/examplePlugin.py
+++ plugins/examplePlugin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
+import random
 from plugin import *
 
 class examplePlugin(Plugin):
@@ -14,7 +14,8 @@ class examplePlugin(Plugin):
             answer = self.ask(u"Willst du das wirklich wissen?")
             self.say(u"Du hast \"{0}\" gesagt!".format(answer))
         elif language == 'fr-FR':
-		    self.say(u"Je ne sais pas, mais je crois qu'il y a une application pour ça.");
+            rep = [u"Je ne sais pas, mais je crois qu'il y a une application pour ça.", "42"]
+            self.say(random.choice(rep))
         else:
             self.say("I shouldn't tell you!")
         self.complete_request()
