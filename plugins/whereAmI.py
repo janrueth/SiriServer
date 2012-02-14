c6c04464b004ef3a5b526f1463474a33c14dabe3
john-dev <joh.dev@gmail.com>
2012-02-14 19:21:46 +0100

    Using Location instead if SiriLocation Added support for "where is <city/country>"
    The "fixed" Version of Location is required first! This won't work without this fix..

--- plugins/whereAmI.py
+++ plugins/whereAmI.py
@@ -10,12 +10,10 @@ import json
 from plugin import *
  
 from siriObjects.baseObjects import AceObject, ClientBoundCommand
-from siriObjects.systemObjects import GetRequestOrigin
+from siriObjects.systemObjects import GetRequestOrigin, Location
 from siriObjects.uiObjects import AddViews, AssistantUtteranceView
-from siriObjects.mapObjects import SiriLocation, SiriMapItem, SiriMapItemSnippet
+from siriObjects.mapObjects import SiriMapItem, SiriMapItemSnippet
 
-geonames_user="test2"
- 
 class whereAmI(Plugin):
     
     @register("de-DE", "(Wo bin ich.*)")    
@@ -35,7 +33,7 @@ class whereAmI(Plugin):
                 components = response['results'][0]['address_components']              
                 street = filter(lambda x: True if "route" in x['types'] else False, components)[0]['long_name']
                 stateLong= filter(lambda x: True if "administrative_area_level_1" in x['types'] or "country" in x['types'] else False, components)[0]['long_name']
-                postalCode= filter(lambda x: True if "postal_code" in x['types'] else False, components)[0]['long_name']
+                postalCode=""
                 try:
                     city = filter(lambda x: True if "locality" in x['types'] or "administrative_area_level_1" in x['types'] else False, components)[0]['long_name']
                 except:
@@ -46,8 +44,8 @@ class whereAmI(Plugin):
                     the_header="Dein Standort"
                 else:
                     the_header="Your location"
-                Location=SiriLocation(the_header, street, city, stateLong, countryCode, postalCode, latitude, longitude)
-                mapsnippet = SiriMapItemSnippet(items=[SiriMapItem(the_header, Location)])
+                s_Location=Location(the_header, street, city, stateLong, countryCode, postalCode, latitude, longitude)
+                mapsnippet = SiriMapItemSnippet(items=[SiriMapItem(the_header, s_Location)])
                 view.views = [AssistantUtteranceView(text=the_header, dialogIdentifier="Map"), mapsnippet]
                 self.sendRequestWithoutAnswer(view)
             else:
@@ -61,3 +59,60 @@ class whereAmI(Plugin):
             else:
                 self.say('Could not establish a conenction to Googlemaps','Error');
         self.complete_request()
+
+    @register("de-DE", "(Wo liegt.*)")    
+    @register("en-US", "(Where is.*)")
+    def whereIs(self, speech, language):
+        if language == "de-DE":
+            the_location = re.match("(?u).* liegt ([\w ]+)$", speech, re.IGNORECASE)
+        else:
+            the_location = re.match("(?u).* is ([\w ]+)$", speech, re.IGNORECASE)
+        if the_location != None:
+            the_location = the_location.group(1).strip()
+            the_location = the_location[0].upper()+the_location[1:]
+        else:
+            if language == "de-DE":
+                self.say('Ich habe keinen Ort gefunden!',None)
+            else:
+                self.say('No location found!',None)
+            self.complete_request()  
+        url = u"http://maps.googleapis.com/maps/api/geocode/json?address={0}&sensor=false&language={1}".format(urllib.quote_plus(the_location.encode("utf-8")), language)
+        jsonString=None
+        try:
+            jsonString = urllib2.urlopen(url, timeout=3).read()
+        except:
+            pass
+        if jsonString != None:
+            response = json.loads(jsonString)
+            if response['status'] == 'OK':
+                location = response['results'][0]['geometry']['location']
+                city=response['results'][0]['address_components'][0]['long_name']
+                try:
+                    country=response['results'][0]['address_components'][2]['long_name']
+                    countryCode=response['results'][0]['address_components'][2]['short_name']
+                except:
+                    country=the_location
+                    countryCode=the_location
+                if language=="de-DE":
+                    the_header=u"Hier liegt {0}".format(the_location)
+                else:
+                    the_header=u"Here is {0}".format(the_location)
+                view = AddViews(self.refId, dialogPhase="Completion")
+                s_Location=Location(the_header, city, city, "", countryCode, "", str(location['lat']), str(location['lng']))
+                mapsnippet = SiriMapItemSnippet(items=[SiriMapItem(the_header, s_Location, "BUSINESS_ITEM")])
+                view.views = [AssistantUtteranceView(text=the_header, dialogIdentifier="Map"), mapsnippet]
+                self.sendRequestWithoutAnswer(view)
+            else:
+                if language=="de-DE":
+                    self.say('Die Googlemaps informationen waren ungenügend!','Fehler')
+                else:
+                    self.say('The Googlemaps response did not hold the information i need!','Error')
+        else:
+            if language=="de-DE":
+                self.say('Ich konnte keine Verbindung zu Googlemaps aufbauen','Fehler')
+            else:
+                self.say('Could not establish a conenction to Googlemaps','Error');
+        self.complete_request()        
+        
+        
+                       
