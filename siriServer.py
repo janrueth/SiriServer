f914e352ebdfc2ed4876fd01cbae8ead39b1a07d
Jimmy Kane <jimmykane9@gmail.com>
2012-02-22 20:23:21 +0200

    fixes

--- siriServer.py
+++ siriServer.py
@@ -314,7 +314,8 @@ class HandleConnection(ssl_dispatcher):
                 elif reqObject['class'] == 'CreateAssistant':
                     #create a new assistant
                     helper = Assistant() 
-                    helper.assistantId=str.upper(str(uuid.uuid4()))                   
+                    helper.assistantId=str.upper(str(uuid.uuid4())) 
+                    helper.speechId=str.upper(str(uuid.uuid4())) 
                     c = self.dbConnection.cursor()
                     noError = True
                     try:
