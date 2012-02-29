19c69cfce887a295c4ae5620fc8b987dbcf3f1ff
Jimmy Kane <jimmykane9@gmail.com>
2012-02-29 02:54:42 +0200

    fixed commit

--- db.py
+++ db.py
@@ -35,13 +35,12 @@ else:
 
 def setup():
     conn = getConnection()
-    c = conn.cursor()
-    if db_type == "mysql":                
-        if db_type == "mysql":
-            c.execute("CREATE DATABASE IF NOT EXISTS googlesiri DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;")
-            c.execute("CREATE TABLE IF NOT EXISTS `assistants` (`id` int(255) unsigned NOT NULL AUTO_INCREMENT,`assistantId` text NOT NULL,`speechId` text NOT NULL,`censorSpeech` text NOT NULL,`timeZoneId` text NOT NULL,`language` text NOT NULL,`region` text NOT NULL,`firstName` text NOT NULL,`nickName` text NOT NULL,`date_created` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;")    
-        else:
-            c.execute("create table if not exists assistants(assistantId text primary key, assistant assi)")
+    c = conn.cursor()                    
+    if db_type == "mysql":
+        c.execute("CREATE DATABASE IF NOT EXISTS googlesiri DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;")
+        c.execute("CREATE TABLE IF NOT EXISTS `assistants` (`id` int(255) unsigned NOT NULL AUTO_INCREMENT,`assistantId` text NOT NULL,`speechId` text NOT NULL,`censorSpeech` text NOT NULL,`timeZoneId` text NOT NULL,`language` text NOT NULL,`region` text NOT NULL,`firstName` text NOT NULL,`nickName` text NOT NULL,`date_created` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;")    
+    else:
+        c.execute("create table if not exists assistants (assistantId text primary key, assistant assi)")
     conn.commit()
     c.close()
     conn.close()
