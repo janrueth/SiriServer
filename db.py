0de6c96aa1b7422d94befea5e82e06fac2b92a9d
Jimmy Kane <jimmykane9@gmail.com>
2012-02-29 04:48:53 +0200

    default=mysql

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ import uuid
 #Declate your database sqlite or mysql
 #In Order to use sqlite please uncomment the sqlite line or just change the mysql name to sqlite
 #db_type = "sqlite"
-db_type = "sqlite"
+db_type = "mysql"
 
 __database__ = "database.sqlite3"
 
