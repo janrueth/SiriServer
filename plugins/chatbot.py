307f4e2ec425638746269cc2a0b773d8caa63f41
Jimmy Kane <jimmykane9@gmail.com>
2012-03-02 01:46:04 +0200

    fixed errors

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -18,30 +18,33 @@ botID = "a9481f8c7e347656"
 
 def askBOT(input):	
 	#convert symbols to HEX
-	input = input.replace(' ', '%20')
-	input = input.replace('?', '%3F')
-	input = input.replace('$', '%24')
-	input = input.replace('+', '%2B')
-	input = input.replace(',', '%2C')
-	input = input.replace('/', '%2F')
-	input = input.replace(':', '%3A') 
-	input = input.replace(';', '%3B') 
-	input = input.replace('=', '%3D') 
-	input = input.replace('@', '%40')	
-	file = urllib2.urlopen('http://www.pandorabots.com/pandora/talk-xml?botid=%s&input=%s' % (botID, input))	
-	data = file.read()	
-	file.close()	
-	dom = parseString(data)	
-	xmlTag = dom.getElementsByTagName('that')[0].toxml()	
-	xmlData=xmlTag.replace('<that>','').replace('</that>','')
-	#convert symbols
-	xmlData = xmlData.replace('&quot;', '"')
-        xmlData = xmlData.replace('&lt;', '<')
-        xmlData = xmlData.replace('&gt;', '>')
-        xmlData = xmlData.replace('&amp;', '&')
-        xmlData = xmlData.replace('<br>', ' ')
-        xmlData = xmlData.replace('Eve', 'Siri')
-	return xmlData
+        try:        
+            input = input.replace(' ', '%20')
+            input = input.replace('?', '%3F')
+            input = input.replace('$', '%24')
+            input = input.replace('+', '%2B')
+            input = input.replace(',', '%2C')
+            input = input.replace('/', '%2F')
+            input = input.replace(':', '%3A') 
+            input = input.replace(';', '%3B') 
+            input = input.replace('=', '%3D') 
+            input = input.replace('@', '%40')	
+            file = urllib2.urlopen('http://www.pandorabots.com/pandora/talk-xml?botid=%s&input=%s' % (botID, input))	
+            data = file.read()	
+            file.close()	
+            dom = parseString(data)	
+            xmlTag = dom.getElementsByTagName('that')[0].toxml()	
+            xmlData=xmlTag.replace('<that>','').replace('</that>','')
+            #convert symbols
+            xmlData = xmlData.replace('&quot;', '"')
+            xmlData = xmlData.replace('&lt;', '<')
+            xmlData = xmlData.replace('&gt;', '>')
+            xmlData = xmlData.replace('&amp;', '&')
+            xmlData = xmlData.replace('<br>', ' ')
+            xmlData = xmlData.replace('Eve', 'Siri')
+            return xmlData
+        except:
+            return 'Sorry can you say that again, please ?'
 
 def respond(self, input):
     if input == 'Stop':
