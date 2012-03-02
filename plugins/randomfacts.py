fce4b5d17e89b5200fa2971c9b0f77c2f1480e61
Jimmy Kane <jimmykane9@gmail.com>
2012-03-02 05:27:34 +0200

    added random facts

---
+++ plugins/randomfacts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# randomfacts.py
+#by Nate Li
+#based on Casey "Nurfballs" Mullineaux's cat fact plugin
+
+from random import randint
+from plugin import *
+import os, random
+
+
+
+class randomfacts(Plugin):
+
+    @register("en-US","(.*random fact.*)|(.*something *.awesome.*)")
+    def st_catfact(self, speech, language):
+        if language == 'en-US':
+            filename = "./plugins/randomfacts.txt"
+            file = open(filename, 'r')
+
+            #Get the total file size
+            file_size = os.stat(filename)[6]
+
+            #Seek to a place int he file which is a random distance away
+            #Mod by the file size so that it wraps around to the beginning
+            file.seek((file.tell()+random.randint(0, file_size-1))%file_size)
+    
+            #Dont use the first readline since it may fall in the middle of a line
+            file.readline()
+
+            #this will return the next (complete) line from the file
+            line = file.readline()
+    
+            #here is the random line
+            self.say(line) 
+             
+        self.complete_request()
+
+
