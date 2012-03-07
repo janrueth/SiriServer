f9c73455dab8615dda2f62a9ce53451054ee5039
codedev <dean.london.jackson@gmail.com>
2012-03-07 16:59:48 +0000

    added en-GB

--- plugins/chucknorris.py
+++ plugins/chucknorris.py
@@ -3,10 +3,19 @@ import json
 
 from plugin import *
 class chucknorrisjoke(Plugin):
-
+	
+        @register ("en-GB", ".*Chuck.*norris.*joke.*")
 	@register ("en-US", ".*Chuck.*norris.*joke.*")
 	def st_chucknorris(self, speech, language):
 		if language == 'en-US':
+			req = urllib2.Request("http://api.icndb.com/jokes/random")
+		full_json = urllib2.urlopen(req).read()
+		load = json.loads(full_json)
+		store = load['value']['joke']
+		store = store.replace('&quot;','\"')
+		self.say(store)
+		self.complete_request()
+		if language == 'en-GB':
 			req = urllib2.Request("http://api.icndb.com/jokes/random")
 		full_json = urllib2.urlopen(req).read()
 		load = json.loads(full_json)
