bf080de0191d31c74054f48aae51947de864c753
Jimmy Kane <jimmykane9@gmail.com>
2012-03-08 19:25:22 +0200

    added wake up for english

--- plugins/alarmPlugin.py
+++ plugins/alarmPlugin.py
@@ -93,3 +93,64 @@ class alarmPlugin(Plugin):
         view.views = [view1, view2]
         self.sendRequestWithoutAnswer(view)
         self.complete_request()
+        
+        
+    wake = {
+        'wakeup': {
+            'en-US': '.*wake.* up at.* (0?[1-9]|1[012])([0-5]\d)?\s?([APap][mM])\s?(\bcalled|named|labeled\b)?\s?(([a-z0-9]{1,7}\s)?([a-z0-9]{1,7})\s?([a-z0-9]{1,7}))?',
+            
+        }
+    }
+
+    @register("en-US", wake['wakeup']['en-US'])    
+    def wakeup(self, speech, language):
+        alarmString = re.match(alarmPlugin.wake['wakeup'][language], speech, re.IGNORECASE)
+
+        if language == 'fr-FR':
+            labelGroupId = 8
+            alarmHour = int(alarmString.group(4))
+            alarm24Hour = alarmHour
+            alarmMinutes = alarmString.group(6)
+            alarmAMPM = ""
+            alarmLabelExists = alarmString.group(labelGroupId)
+        else:
+            labelGroupId = 4
+            alarmHour = int(alarmString.group(1))
+            alarm24Hour = alarmHour
+            alarmMinutes = alarmString.group(2)
+            alarmAMPM = alarmString.group(3)
+            alarmLabelExists = alarmString.group(labelGroupId)
+
+        #check if we are naming the alarm
+        if alarmLabelExists == None:
+            alarmLabel = None
+        else:
+            alarmLabel = alarmString.group(labelGroupId)
+
+        #the siri alarm object requires 24 hour clock
+        if (alarmAMPM == "pm" and alarmHour != 12):
+            alarm24Hour += 12
+        if alarmMinutes == None:
+            alarmMinutes = "00"
+        else:
+            alarmMinutes = int(alarmMinutes.strip())
+        view = AddViews(self.refId, dialogPhase="Reflection")
+        view.views = [
+            AssistantUtteranceView(
+                speakableText=alarmPlugin.localizations['Alarm']['settingAlarm'][language],
+                dialogIdentifier="Alarm#settingAlarm")]
+        self.sendRequestWithoutAnswer(view)
+
+        #create the alarm
+        alarm = AlarmObject(alarmLabel, int(alarmMinutes), alarm24Hour, None, 1)
+        response = self.getResponseForRequest(AlarmCreate(self.refId, alarm))
+        #print(alarmPlugin.localizations['Alarm']['alarmWasSet'][language].format(alarmHour, alarmMinutes, alarmAMPM))
+        view = AddViews(self.refId, dialogPhase="Completion")
+        if alarmLabel == None:
+            view1 = AssistantUtteranceView(speakableText=alarmPlugin.localizations['Alarm']['alarmWasSet'][language].format(alarmHour, alarmMinutes, alarmAMPM), dialogIdentifier="Alarm#alarmWasSet")
+        else:
+            view1 = AssistantUtteranceView(speakableText=alarmPlugin.localizations['Alarm']['alarmSetWithLabel'][language].format(alarmLabelExists, alarmLabel, alarmHour, alarmMinutes, alarmAMPM), dialogIdentifier="Alarm#alarmSetWithLabel")
+        view2 = AlarmSnippet(alarms=[alarm])
+        view.views = [view1, view2]
+        self.sendRequestWithoutAnswer(view)
+        self.complete_request()
