3b2036ea566c6604073ba83f10ec826ea1ebab49
Jimmy Kane <jimmykane9@gmail.com>
2012-03-17 16:26:57 +0200

    fixed keyerrors

--- plugins/horoscope.py
+++ plugins/horoscope.py
@@ -16,11 +16,16 @@ class horoscope(Plugin):
 	@register ("en-US", "(Tell me the horoscope for [a-zA-Z]+)|(Horoscope for [a-zA-Z]+)|(Horoscope for [a-zA-Z]+)|(The horoscope for [a-zA-Z]+)")
 	@register ("fr-FR", "(Quel est mon horoscope pour [a-zA-Z]+)|(Horoscope pour [a-zA-Z]+)|(Horoscope pour [a-zA-Z]+)|(Votre horoscope pour [a-zA-Z]+)")
 	def horoscope_zodiac(self, speech, language):
-	    zodiac = speech.replace('Quel est ','').replace('votre ','').replace('Votre ', '').replace('horoscope','').replace('pour ', '').replace('Horoscope ','').replace(' ','')
+	    zodiac = speech.replace('Quel est ','').replace('votre ','').replace('Votre ', '').replace('horoscope','').replace('Horoscope','').replace('pour ', '').replace('Tell me the','').replace('for','').replace('The','').replace(' ','')
+            print ("Zodiac: {0}".format(zodiac))
             linkurl = 'http://widgets.fabulously40.com/horoscope.json?sign=%s' % zodiac
             print linkurl
 	    req=urllib.urlopen(linkurl)
             full_json=req.read()
 	    full=json.loads(full_json)
-	    self.say(full['horoscope']['horoscope'])
-	    self.complete_request()
+            try:
+                self.say(full['horoscope']['horoscope'])
+                
+            except KeyError: 
+                self.say("Sorry I did not find a horoscope for zodiac {0}".format(zodiac))
+            self.complete_request()
